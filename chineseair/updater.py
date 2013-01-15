c1e5484b9f9f530e7ca043c432bc1cc09a442e11
Aengus Walton <ventolin@gmail.com>
2013-01-15 23:34:07 +0000

    Draw graph for all data, refactor updater in order to do this

--- chineseair/updater.py
+++ chineseair/updater.py
@@ -17,7 +17,7 @@ API = twitter.Api(**AUTH)
 
 def update_webpage():
     print 'Updating webpage...'
-    datapoint = '''[new Date({year}, {month}, {date}, {hour}, 0, 0), {value}]'''
+    datapoint = '''[new Date({year}, {month}, {date}, {hour}, 0, 0), {beijing}, {shanghai}, {chengdu}, {guangzhou}]'''
     template = '''
         data.addRows([
             {datapoints}
@@ -25,16 +25,18 @@ def update_webpage():
         return data;
     '''
     print 'Loading data...'
-    with open('beijingair.json', 'r') as f:
+    with open('data.json', 'r') as f:
         data = json.loads(f.read())
     datapoints = []
     print 'Constructing HTML...'
-    for entry in data:
-        for timestr, value in entry.iteritems():
-            t = time.strptime(timestr, '%m-%d-%Y %H:%M')
-            datapoints.append(datapoint.format(year=t.tm_year, month=t.tm_mon - 1,
-                                               date=t.tm_mday, hour=t.tm_hour,
-                                               value=value))
+    for timestr, values in data.iteritems():
+        t = time.strptime(timestr, '%m-%d-%Y %H:%M')
+        datapoints.append(datapoint.format(year=t.tm_year, month=t.tm_mon - 1,
+                                           date=t.tm_mday, hour=t.tm_hour,
+                                           beijing=values.get('beijingair', 'undefined'),
+                                           shanghai=values.get('CGShanghaiAir', 'undefined'),
+                                           chengdu=values.get('CGChengduAir', 'undefined'),
+                                           guangzhou=values.get('Guangzhou_Air', 'undefined')))
     datapoints = ',\n'.join(datapoints)
     html = '''
         <script type="text/javascript" id="chineseair_data_population">
@@ -53,18 +55,23 @@ def update_webpage():
     print 'Done!'
 
 
-def update_feed(feed):
-    filename = '%s.json' % feed
+def update_feeds():
+    filename = 'data.json'
     if not os.path.exists(filename):
-        total_data = []
+        total_data = {}
     else:
         with open(filename, 'r') as f:
             total_data = json.loads(f.read())
-    finished = False
+
+    finished = {}
+    for feed in FEEDS:
+        finished[feed] = False
     page = 1
-    while not finished:
-        print 'Getting page {0} of tweets for {1}'.format(page, feed)
-        finished, total_data = get_new_tweets(feed, page, total_data)
+
+    while not all([state for _, state in finished.iteritems()]):
+        for feed in FEEDS:
+            print 'Getting page {0} of tweets for {1}'.format(page, feed)
+            finished[feed], total_data = get_new_tweets(feed, page, total_data)
         page += 1
 
     print 'Saving %s' % filename
@@ -79,15 +86,18 @@ def get_new_tweets(feed, page, total_data):
     finished = False
     for tweet in tweets:
         try:
-            tweet = process_tweet(tweet)
+            time, value = process_tweet(tweet)
         except ValueError:
             continue
 
-        if tweet in total_data:
-            finished = True
-            break
+        if time in total_data:
+            if feed in total_data[time]:
+                finished = True
+                break
+            else:
+                total_data[time][feed] = value
         else:
-            total_data.append(tweet)
+            total_data[time] = {feed: value}
     return finished, total_data
 
 
@@ -97,10 +107,9 @@ def process_tweet(tweet):
         raise ValueError('Tweet is an average value')
     if 'No Reading' in tweet.text or 'No Data' in tweet.text or len(processed) < 5:
         raise ValueError('Tweet contained no value')
-    return {processed[0]: processed[3]}
+    return processed[0], processed[3]
 
 
 if __name__ == '__main__':
-    for feed in FEEDS:
-        update_feed(feed)
+    update_feeds()
     update_webpage()
