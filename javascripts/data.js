1f1b94e5efe2614dbac2c8f10664149c06ea5078
Aengus Walton <ventolin@gmail.com>
2013-08-01 16:10:14 +0100

    Updated by run at Thu Aug  1 16:10:14 IST 2013

--- javascripts/data.js
+++ javascripts/data.js
@@ -9012,6 +9012,7 @@
 [new Date(2013, 7, 1, 2, 0, 0), 91, 78, 163, undefined],
 [new Date(2013, 7, 1, 20, 0, 0), 153, undefined, 74, undefined],
 [new Date(2013, 7, 1, 21, 0, 0), 149, undefined, 76, undefined],
+[new Date(2013, 7, 1, 22, 0, 0), 157, 74, 95, undefined],
 [new Date(2013, 7, 1, 3, 0, 0), 91, 82, 171, undefined],
 [new Date(2013, 7, 1, 4, 0, 0), 107, 93, 169, undefined],
 [new Date(2013, 7, 1, 5, 0, 0), 117, 112, 166, undefined],
